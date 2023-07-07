b9ceaed9f3a53d53ae0586f857e34843e40450b7
TiesHogenboom <70697022+TiesHogenboom@users.noreply.github.com>
2023-07-07 13:49:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Stage Blog </h1>
+      <h3> Week 1 / 2
+         Feb 13 - Feb 24</h3>
+
+      <p>Op Februari 13 begon ik met mijn stage bij Moodus. Het begin was wel heel erg veel wennen, om van een school ritme naar een echt bedrijf ritme te gaan is een 
+heel verschil. Gelukkig ging dat goed en voelde ik mij thuis in het Moodus team. Ik begon eerst met de planning van mijn stage, een trello opzetten en de 
+OnStage documenten in te vullen. Ik ben ook bezig met research over wat ik wil gaan verbeteren in mijn tijd bij moodus. Ik heb plezier elke dag en zie uit naar 
+de komende weken van mijn stage!</p>
+
+      <h3> Week 3 / 4
+         Feb 27 - Mrt 10</h3>
+
+      <p>De afgelopen 2 weken heb ik de PAP ingevuld met mijn stage begeleider en mijn stage blog ingeleverd. Het gaat nog steeds goed, ik ben van plan om de aankomende
+weken te werken aan mijn React skills zodat ik kan beginnen aan een project van Moodus wat nu nog openstaat. Het project is een dashboard voor alle foutmeldingen 
+van de terminals. Ik heb geprobeerd om in het project te komen maar daar had ik wat problemen mee dus heb ik bij Alejandro ( codeerder van Moodus ) 
+de code gezien en door het project gelopen. Ik moet nog veel leren om er aan kunnen te werken dus daar ga ik nu aan verder.</p>
+
+      <h3> Week 5 / 6
+         Mrt 13 - Mrt 24</h3>
+
+      <p>Het gaat goed met het leren van React, ik ben zelf bezig met wat projecten en tutorials van youtube. Op kantoor is het erg druk met de voorbereiding op de ramadan
+omdat veel van de clienten van Moodus moskee's zijn. Ik werk veel op mijn zelf.</p>
+
+      <h3> Week 7 / 8
+         Mrt 27 - Apr 7</h3>
+
+      <p>Ik had mijn eerste dag ziek in deze maand, op werk is het erg druk met de Ramadan en ik heb gehoord dat Alejandro weg gaat. Ik ga een plan maken over 
+wat ik ga doen zonder Alejandro op werk omdat ik dan de enigste developer op werk ben. Voor de rest ben ik nog steeds bezig in React.</p>
+
+      <h3> Week 9 / 10
+         Apr 5 - Apr 21</h3>
+
+      <p>Ik zat in het begin van deze 2 weken heel erg vast met wat ik wou doen voor projecten als ik niet de hulp van Alejandro krijg. Ook was de sfeer op werk wat
+minder. Dus heb ik contact opgenomen met Milos en een gesprek over stage gehad, en na het gesprek had ik een goed idee van wat ik de rest van mijn stage zal
+doen. Ik ben van plan om nog een tijdje door te gaan met het zelf leren, en hopelijk kan ik nog een aan project werken voordat mijn stage eindigt. Op werk
+hadden we een team event op Alejandro's laatste dag.</p>
+
+      <h3> Week 11 / 12
+         Apr 24 - Mei 5</h3>
+
+      <p>Ik begon deze 2 weken met een kleine vakantie in belgië, en het vieren van koningsdag wat erg gezellig was samen met een werk collega. Ik ben nog steeds bezig
+met React maar zit wel met Tim te kijken dat ik snel aan een project ga werken.</p>
+
+      <h3> Week 13 / 14
+         Mei 8 - Mei 19</h3>
+
+      <p>Er is een bedrijf ingehuurd om de website van Moodus te verbeteren, voor mij was dit net een te groot project om aan te beginnen op dit punt van mijn stage. 
+Ik vond het wel erg interresant om te zien hoe het proces ging, ik heb bij elke meeting mee gekeken en kan de design volgen door de gedeelde bestanden. Op werk
+was het weer wat rustiger.
+</p>
+
+      <h3> Week 15 / 16
+         Mei 22 - Jun 2</h3>
+
+      <p>Ik heb nog wat meer meetings gehad met de website makers, en ben nu klaar met het werken aan react, dus ga aan een project beginnen, Tim heeft mij een project
+gegeven voor een support email, die gebruikt zal worden door de productie team. Daar ga ik volgende week aan beginnen.</p>
+
+      <h3> Week 17 / 18
+         Jun 5 - Jun 16</h3>
+
+      <p>De support email voor Moodus is af en werkend, was erg fijn om aan een project te beginnen. Heb contact gehad met Mohammed (freelance developer in turkije) om
+aan het error dashboard project te beginnen. Ik heb hier met Alejandro al naar gekeken op het begin van mijn stage maar toen was mijn React level nog niet
+goed genoeg om er aan te werken. Ik had weer last met het openen van het project en alles te instaleren, en Mohammed was druk bezig met een ander project dus 
+uiteindelijk ben ik er niet aan begonnen en ga ik starten met mijn eigen project. Ik wil een checklist website maken voor het productie team, om hun te helpen
+met het bouw proces bij te houden. Ik ga hiervoor ook Firebase leren omdat ik ook back-end nodig heb. Ik hoop dat ik voor het einde van mijn stage dit project
+af krijg.</p>
+
+      <h3> Week 19 / 20
+         Jun 19 - Jun 30</h3>
+
+      <p>Deze 2 weken begon ik het maken van de checklist website na het inleveren van een plan van aanpak. Ik begon met het maken van een wireframe, en ben toen verder
+gegaan en met het maken van de UI design. Na de designs ben ik gelijk gestart met het coderen, ik begon met een tutorial voor firebase en vanuit daar heb ik de
+website opgebouwd. Omdat firebase erg nieuw voor mij is kom ik tegen veel errors en bugs aan dus daar ben ik nu druk mee bezig om het op te lossen.</p>
+
+      <h3> Week 21 / 22
+         Jul 3 - Jul 14</h3>
+
+      <p></p>
+
+      
+    </>
   );
 }
 
